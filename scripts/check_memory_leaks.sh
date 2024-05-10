0e25a2edf9d3ed58c922048f4f6f5540aadb04f7
Quinton Erasmus <qcerasmus@gmail.com>
2024-05-10 23:05:13 +0200

    added valgrind script

---
+++ scripts/check_memory_leaks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Please provide the program name."
+  exit 1
+fi
+
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose $1
+

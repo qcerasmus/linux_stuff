5843b3c786ee3e4632475a866b697b6c857a7a74
Quinton Erasmus <qcerasmus@gmail.com>
2025-07-06 14:46:49 +0200

    Added unit testing by default as well

--- scripts/make_cmake_project.sh
+++ scripts/make_cmake_project.sh
@@ -22,9 +22,15 @@ project(${project_name})
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(TESTING ON)
 
 add_subdirectory(third_party)
 include_directories(\${spdlog_SOURCE})
+if (\${TESTING})
+  enable_testing()
+  include_directories(\${doctest_SOURCE})
+  add_subdirectory(testing)
+endif()
 add_subdirectory(console)
 " >> CMakeLists.txt
 
@@ -32,6 +38,7 @@ mkdir third_party
 touch third_party/CMakeLists.txt
 echo "include(FetchContent)
 
+message(STATUS \"downloading spdlog\")
 FetchContent_Declare(
   spdlog
   GIT_REPOSITORY https://github.com/gabime/spdlog.git
@@ -43,6 +50,21 @@ FetchContent_MakeAvailable(spdlog)
 
 add_library(spdlog INTERFACE)
 set(spdlog_SOURCE \${spdlog_SOURCE_DIR}/include PARENT_SCOPE)
+
+if(\${TESTING})
+  FetchContent_Declare(
+    doctest
+    GIT_REPOSITORY https://github.com/doctest/doctest.git
+    GIT_TAG v2.4.12
+    GIT_SHALLOW true
+    SOURCE_SUBDIR paththatdoesnotexist
+  )
+  message(STATUS \"downloading doctest\")
+  FetchContent_MakeAvailable(doctest)
+
+  add_library(doctest INTERFACE)
+  set(doctest_SOURCE \${doctest_SOURCE_DIR}/doctest PARENT_SCOPE)
+endif()
 " >> third_party/CMakeLists.txt
 
 mkdir console
@@ -64,6 +86,23 @@ int main()
     spdlog::debug(\"Hello, World!\");
 }
 " >> console/main.cpp
+
+mkdir testing
+touch testing/${project_name}.cpp
+echo "#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include \"doctest.h\"
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE(\"testing the factorial function\") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
+}" >> testing/${project_name}.cpp
+
+echo "add_executable(${project_name}_test ${project_name}.cpp)
+add_test(${project_name}_test ${project_name}_test)" >> testing/CMakeLists.txt
 
 cp ~/linux_stuff/configs/.clang-format .
 git init --initial-branch=main
